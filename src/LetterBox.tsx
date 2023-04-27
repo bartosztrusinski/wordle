78e784e6c937f117207dae1c3a5487e3811fbdee
Bartosz Trusiński <bartosztrusinski@gmail.com>
2023-04-27 23:56:27 +0200

    add styles & spot prop

--- src/LetterBox.tsx
+++ src/LetterBox.tsx
@@ -2,20 +2,30 @@ import styled from 'styled-components';
 
 interface LetterBoxProps {
   letter: string;
+  spot?: 'correct' | 'wrong' | 'none';
 }
 
 const Box = styled.div`
-  border: 0.125rem solid #3a3a3c;
-  font-size: 2rem;
+  display: flex;
+  place-content: center;
+  place-items: center;
+  aspect-ratio: 1;
+  line-height: 1;
   font-weight: bold;
   user-select: none;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  border: ${({ spot, letter }: LetterBoxProps) =>
+    spot ? '0' : `0.125rem solid var(--color-${letter ? 'gray' : 'none'})`};
+  text-transform: uppercase;
+  background-color: ${({ spot }: LetterBoxProps) =>
+    `var(--color-${spot ?? 'dark'})`};
 `;
 
-const LetterBox = ({ letter }: LetterBoxProps) => {
-  return <Box>{letter}</Box>;
+const LetterBox = ({ letter, spot }: LetterBoxProps) => {
+  return (
+    <Box spot={spot} letter={letter}>
+      {letter}
+    </Box>
+  );
 };
 
 export default LetterBox;
