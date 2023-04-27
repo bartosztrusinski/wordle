7564bd3902b269812ff2ff7ab6f5b05303c393da
Bartosz Trusiński <bartosztrusinski@gmail.com>
2023-04-28 00:01:54 +0200

    create utilities

---
+++ src/util.ts
@@ -0,0 +1,55 @@
+import { Letter, Word } from './interface';
+import validWords from './validWords.json';
+import solutions from './solutions.json';
+
+const keyboard = [
+  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+  ['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'backspace'],
+];
+
+const maxGuesses = 6;
+const wordLength = 5;
+
+const getCSSVariable = (name: string) =>
+  getComputedStyle(document.documentElement).getPropertyValue(name);
+
+const setCSSVariable = (name: string, value: string) =>
+  document.documentElement.style.setProperty(name, value);
+
+const initWords = (): Word[] =>
+  Array(maxGuesses).fill(Array(wordLength).fill(''));
+
+const getRandomSolution = () => {
+  const randomIndex = Math.floor(Math.random() * solutions.length);
+  return solutions[randomIndex];
+};
+
+const createLetterCount = (word: string) =>
+  word.split('').reduce((acc, letter) => {
+    if (acc[letter]) {
+      acc[letter]++;
+    } else {
+      acc[letter] = 1;
+    }
+
+    return acc;
+  }, {} as Record<Letter, number>);
+
+const isValidLetter = (letter: Letter) =>
+  keyboard.flat().includes(letter) && letter.length === 1;
+
+const isValidWord = (word: string) => validWords.includes(word);
+
+export {
+  keyboard,
+  maxGuesses,
+  wordLength,
+  getCSSVariable,
+  setCSSVariable,
+  initWords,
+  getRandomSolution,
+  createLetterCount,
+  isValidLetter,
+  isValidWord,
+};
